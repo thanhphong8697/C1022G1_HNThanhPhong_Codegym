194279d8addbbf82f55234c546bfffd013886c9f
thanhphong8697 <Thanhphong861997@gmail.com>
2023-01-13 17:01:46 +0700

    update

--- module_3/casestudy/furama_database/access_furama.sql
+++ module_3/casestudy/furama_database/access_furama.sql
@@ -1,1 +1,53 @@
 USE furama_database;
+SELECT 
+    ma_nhan_vien, ho_ten
+FROM
+    nhan_vien
+WHERE
+    SUBSTRING_INDEX(ho_ten, ' ', - 1) LIKE 'H%'
+        OR SUBSTRING_INDEX(ho_ten, ' ', - 1) LIKE 'T%'
+        OR SUBSTRING_INDEX(ho_ten, ' ', - 1) LIKE 'K%'
+        AND CHAR_LENGTH(ho_ten) <= 15;
+SELECT 
+    ma_khach_hang, ho_ten, dia_chi, ngay_sinh
+FROM
+    khach_hang
+WHERE
+    (YEAR(CURDATE()) - YEAR(ngay_sinh)) BETWEEN 18 AND 50
+        AND dia_chi LIKE '%Đà Nẵng'
+        OR dia_chi LIKE '%Quảng Trị';
+
+SELECT 
+    khach_hang.ma_khach_hang,
+    ho_ten,
+    COUNT(khach_hang.ma_khach_hang) AS so_lan_dat_phong
+FROM
+    khach_hang
+        INNER JOIN
+    hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+        INNER JOIN
+    loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+WHERE
+    loai_khach.ten_loai_khach = 'Diamond'
+GROUP BY khach_hang.ma_khach_hang
+ORDER BY khach_hang.ma_khach_hang;
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SELECT 
+    khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
+    SUM(IFNULL(dich_vu.chi_phi_thue, 0) + IFNULL(hop_dong_chi_tiet.so_luong, 0) * IFNULL(dich_vu_di_kem.gia, 0)) AS tong_tien
+FROM
+    khach_hang
+        LEFT JOIN
+    loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+        LEFT JOIN
+    hop_dong ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    loai_dich_vu ON loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+GROUP BY ma_hop_dong
+ORDER BY ma_khach_hang ASC , ma_hop_dong DESC;
