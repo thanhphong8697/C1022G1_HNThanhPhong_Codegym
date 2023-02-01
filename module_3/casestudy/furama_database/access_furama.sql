8a06d9f693aaeef860b3b379dbef843b3b9d41bf
thanhphong8697 <Thanhphong861997@gmail.com>
2023-02-01 15:53:35 +0700

    update

--- module_3/casestudy/furama_database/access_furama.sql
+++ module_3/casestudy/furama_database/access_furama.sql
@@ -149,6 +149,107 @@ FROM
     hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
 GROUP BY hd.ma_hop_dong;
 
-
-
-
+-- 11. Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+SELECT 
+    dvdk.ma_dich_vu_di_kem,
+	dvdk.ten_dich_vu_di_kem
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+        INNER JOIN
+    hop_dong hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+        INNER JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+        INNER JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+WHERE
+    lk.ten_loai_khach = 'Diamond'
+        AND kh.dia_chi LIKE '% Vinh'
+        OR '% Quảng Ngãi';
+        
+-- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem 
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 
+-- 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+SELECT 
+    hd.ma_hop_dong,
+    nv.ho_ten,
+    kh.ho_ten,
+    kh.so_dien_thoai,
+    dv.ten_dich_vu,
+    SUM(IFNULL(hdct.so_luong, 0)) AS so_luong_dich_vu_di_kem,
+    hd.tien_dat_coc
+FROM
+    hop_dong hd
+        INNER JOIN
+    dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        INNER JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+        LEFT JOIN
+    hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+        INNER JOIN
+    nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+WHERE
+    YEAR(hd.ngay_lam_hop_dong) = 2020
+        AND MONTH(hd.ngay_lam_hop_dong) > 9
+        AND hd.ma_hop_dong NOT IN (SELECT 
+            hop_dong.ngay_lam_hop_dong
+        FROM
+            hop_dong
+        WHERE
+            YEAR(hop_dong.ngay_lam_hop_dong) = 2021
+                AND MONTH(hop_dong.ngay_lam_hop_dong) < 7)
+GROUP BY hd.ma_hop_dong
+ORDER BY hd.ma_hop_dong;
+ 
+ -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+ -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+SELECT 
+    dvdk.ten_dich_vu_di_kem,
+    SUM(IFNULL(hdct.so_luong, 0)) AS so_luong
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY dvdk.ma_dich_vu_di_kem
+HAVING SUM(IFNULL(hdct.so_luong, 0)) >= (SELECT 
+        MAX(hop_dong_chi_tiet.so_luong)
+    FROM
+        hop_dong_chi_tiet);
+        
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem).
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+ select hd.ma_hop_dong, dv.ten_dich_vu, dvdk.ten_dich_vu_di_kem, count(hdct.ma_dich_vu_di_kem) as so_lan_su_dung from hop_dong hd 
+ inner join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+ inner join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+ inner join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+ group by dvdk.ma_dich_vu_di_kem
+ having count(hdct.ma_dich_vu_di_kem) = 1
+ order by hd.ma_hop_dong;
+ 
+ -- 15.Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, 
+ -- dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+SELECT 
+    nv.ma_nhan_vien,
+    nv.ho_ten,
+    td.ten_trinh_do,
+    bp.ten_bo_phan,
+    nv.so_dien_thoai
+FROM
+    nhan_vien nv
+        INNER JOIN
+    hop_dong hd ON hd.ma_nhan_vien = nv.ma_nhan_vien
+        INNER JOIN
+    trinh_do td ON td.ma_trinh_do = nv.ma_trinh_do
+        INNER JOIN
+    bo_phan bp ON bp.ma_bo_phan = nv.ma_bo_phan
+GROUP BY nv.ma_nhan_vien
+HAVING COUNT(nv.ma_nhan_vien) < 4;
+ 
+ 
+ 
+ 
+ 
+ 
