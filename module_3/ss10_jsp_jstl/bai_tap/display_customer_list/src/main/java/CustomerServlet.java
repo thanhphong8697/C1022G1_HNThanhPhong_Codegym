b97d828dec336bffc0a996896d2f7c31cb66cebc
thanhphong8697 <Thanhphong861997@gmail.com>
2023-02-14 14:36:17 +0700

    update

--- module_3/ss10_jsp_jstl/bai_tap/display_customer_list/src/main/java/CustomerServlet.java
+++ module_3/ss10_jsp_jstl/bai_tap/display_customer_list/src/main/java/CustomerServlet.java
@@ -26,11 +26,9 @@ public class CustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("customerList", customerList);
         request.getRequestDispatcher("/customer_list.jsp").forward(request,response);
-
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
